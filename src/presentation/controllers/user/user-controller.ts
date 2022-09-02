41fb5f2d658b080baf9a14ec700361c3f6fd24bd
Breno Vergilio <bjvergilio47@gmail.com>
2022-09-02 12:27:05 -0400

    feat: add all user controller methods

--- src/presentation/controllers/user/user-controller.ts
+++ src/presentation/controllers/user/user-controller.ts
@@ -1,4 +1,8 @@
 import { makeCreateUserUseCase } from "../../../infra/factories/usecases/create-user-use-case-factory";
+import { makeDeleteUserUseCase } from "../../../infra/factories/usecases/delete-user-use-case-factory";
+import { makeFindAllUsersUseCase } from "../../../infra/factories/usecases/find-all-users-use-case-factory";
+import { makeFindUserByIDUseCase } from "../../../infra/factories/usecases/find-user-by-id-use-case-factory";
+import { makeUpdateUserUseCase } from "../../../infra/factories/usecases/update-user-use-case-factory";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { createUserValidations, updateUserValidations } from "./user-controller-validations";
 
@@ -8,7 +12,7 @@ export class UserController {
     const error = createUserValidations().validate(body);
 
     if(error) throw error;
-    
+
     const createUserUseCase = makeCreateUserUseCase();
     const createUserUseCaseResponse = await createUserUseCase.handle(body);
 
@@ -19,15 +23,42 @@ export class UserController {
   }
 
   static async updateUser(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { body } = httpRequest;
+    const { params, body } = httpRequest;
     const error = updateUserValidations(Object.keys(body)).validate(body);
     if(error) throw error;
+
+    const updateUserUseCase = makeUpdateUserUseCase();
+    const updateUserUseCaseResponse = await updateUserUseCase.handle({ id: params.id, ...body });
+
+    if(!updateUserUseCaseResponse) return {
+      statusCode: 404,
+      body: {
+        message: 'Usuário não encontrado'
+      }
+    }
+
+    return {
+      statusCode: 200,
+      body: updateUserUseCaseResponse
+    }
   }
 
   static async findUserByID(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { id } = httpRequest.params;
+    
+    const findUserByIDUseCase = makeFindUserByIDUseCase();
+    const findUserByIDUseCaseResult = await findUserByIDUseCase.handle(id);
+
+    if(!findUserByIDUseCaseResult) return {
+      statusCode: 404,
+      body: {
+        message: 'Usuário não encontrado'
+      }
+    }
+
     return {
       statusCode: 200,
-      body: 'something'
+      body: findUserByIDUseCaseResult
     }
   }
 
@@ -35,12 +66,38 @@ export class UserController {
     const { query } = httpRequest;
     const error = updateUserValidations(Object.keys(query)).validate(query);
     if(error) throw error;
+
+    const findAllUsersUseCase = makeFindAllUsersUseCase();
+    const findAllUsersUseCaseResult = await findAllUsersUseCase.handle({ page: query.page, perPage: query.perPage });
+
+    return {
+      statusCode: 200,
+      body: findAllUsersUseCaseResult
+    }
   }
 
   static async deleteUser(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { id } = httpRequest.params;
+    
+    if(!id) return {
+      statusCode: 400,
+      body: {
+        message: 'ID não foi fornecido'
+      }
+    }
+
+    const deleteUserUseCase = makeDeleteUserUseCase();
+    const deleteUserUseCaseResult = await deleteUserUseCase.handle(id);
+ 
+    if(deleteUserUseCaseResult === null) return {
+      statusCode: 404,
+      body: {
+        message: 'Usuário não encontrado'
+      }
+    }
+
     return {
-      statusCode: 200,
-      body: 'something'
+      statusCode: 204
     }
   }
 }
--- src/usecases/delete-user/delete-user-use-case.ts
+++ src/usecases/delete-user/delete-user-use-case.ts
@@ -3,7 +3,11 @@ import { UserRepository } from "../../domain/users/user-repository";
 export class DeleteUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async handle(id: string): Promise<void> {
+  async handle(id: string): Promise<void | null> {
+    const user = await this.userRepository.findByID(id);
+
+    if(!user) return null;
+    
     await this.userRepository.delete(id);
   }
 }
